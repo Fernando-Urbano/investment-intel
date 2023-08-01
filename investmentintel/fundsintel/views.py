92df8d627b2369834f858b97d30828cee26dbc42
Fernando Rocha Urbano <fernando.rocha.urbano@gmail.com>
2023-08-01 11:40:26 -0300

    added react features

--- investmentintel/fundsintel/views.py
+++ investmentintel/fundsintel/views.py
@@ -5,7 +5,10 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime, date
+from dateutil import relativedelta
 import logging
+import re
 import json
 import pandas as pd
 import sqlite3
@@ -85,18 +88,48 @@ def login_view(request):
 # Get Market Data
 @csrf_exempt
 def get_market_data(request):
+    user = request.user.username if request.user else "anonimous_user"
     data_path = 'fundsintel'
     # Get data
     data = json.loads(request.body)
     cnpjs = data.get("cnpjs")
     data_type = data.get("data_type")
+    period = data.get("period")
     start_dt_comptc = data.get("start_dt_comptc")
     end_dt_comptc = data.get("end_dt_comptc")
-    normalize = data.get("normalize")
+    if period:
+        period = period.lower()
+        months_match = re.search('([0-9]+) month', period)
+        years_match = re.search('([0-9]+) year', period)
+        end_dt_comptc = date.today() if not end_dt_comptc else end_dt_comptc
+        if months_match:
+            delta = int(months_match.group(1))
+            start_dt_comptc = end_dt_comptc - relativedelta.relativedelta(months=delta)
+        elif years_match:
+            delta = int(years_match.group(1))
+            start_dt_comptc = end_dt_comptc - relativedelta.relativedelta(years=delta)
+        elif period == "ytd":
+            if end_dt_comptc.month == 1 and end_dt_comptc.day <= 6:
+                selected_year = end_dt_comptc.year - 1
+            else:
+                selected_year = end_dt_comptc.year
+            start_dt_comptc = date(selected_year, 1, 1)
+        elif period == "mtd":
+            if end_dt_comptc.day <= 6:
+                start_dt_comptc = end_dt_comptc - relativedelta.relativedelta(days=15)
+                start_dt_comptc = start_dt_comptc.replace(day=1)
+            else:
+                start_dt_comptc = end_dt_comptc.replace(day=1)
+    if data_type == "cumulative_returns":
+        data_type = 'quota'
+        normalize = True
+    elif data_type == "returns":
+        data_type = 'quota'
+        pct_diff = True
     # Query
     cnpjs = [cnpjs] if isinstance(cnpjs, str) else cnpjs
     cnpjs_query = "'" + "', '".join(cnpjs) + "'"
-    logging.info(f'Requested {data_type} from {cnpjs_query}')
+    logging.info(f'{user}: Requested {data_type} from {cnpjs_query}')
     conn = sqlite3.connect(data_path + "\\" + "funds_info.db")
     query = f"""
         SELECT cnpj, dt_comptc, {data_type} as data_type
@@ -104,24 +137,37 @@ def get_market_data(request):
         WHERE cnpj in ({cnpjs_query})
     """
     if start_dt_comptc:
-        query += f" AND dt_comptc >= '{start_dt_comptc}'"
+        if isinstance(start_dt_comptc, (datetime, date)):
+            start_dt_comptc = start_dt_comptc.strftime('%Y-%m-%d')  
+        if re.search('[0-9]{4}-[0-9]{2}-[0-9]{2}', start_dt_comptc):  
+            query += f" AND dt_comptc >= '{start_dt_comptc}'"
     if end_dt_comptc:
-        query += f" AND dt_comptc <= '{end_dt_comptc}'"
+        if isinstance(end_dt_comptc, (datetime, date)):
+            end_dt_comptc = end_dt_comptc.strftime('%Y-%m-%d')
+        if re.search('[0-9]{4}-[0-9]{2}-[0-9]{2}', end_dt_comptc):      
+            query += f" AND dt_comptc <= '{end_dt_comptc}'"
     query += " ORDER BY cnpj, dt_comptc"
     market_data = pd.read_sql(query, conn)
-    if normalize:
-        market_data['data_type'] = market_data.groupby('cnpj')['data_type'].transform(lambda x: x / x.iloc[0] - 1)
-    market_data = market_data.pivot(index='dt_comptc', columns='cnpj', values='data_type')
-    market_data = market_data.to_dict()
-    market_data = {cnpj: market_data[cnpj] if cnpj in market_data.keys() else "No data" for cnpj in cnpjs}
+    if len(market_data.index) == 0:
+        market_data = {k: "No data" if k in cnpjs else [] for k in ["dt_comptc"] + cnpjs}
+    else:
+        if normalize:
+            market_data['data_type'] = market_data.groupby('cnpj')['data_type'].transform(lambda x: x / x.iloc[0] - 1)
+        elif pct_diff:
+            market_data['data_type'] = market_data.groupby('cnpj')['data_type'].pct_change()
+            market_data.dropna(subset=['data_type'], inplace=True)
+        market_data = market_data.pivot(index='dt_comptc', columns='cnpj', values='data_type')
+        market_data.reset_index('dt_comptc', inplace=True)
+        market_data = market_data.to_dict(orient='list')
+        market_data = {k: market_data[k] if k in market_data.keys() else "No data" for k in ["dt_comptc"] + cnpjs}
     market_information = {}
-    for cnpj in market_data.keys():
+    for cnpj in cnpjs:
         try:
             fund = InvestmentFund.objects.get(cnpj=cnpj)
             market_information[cnpj] = fund.serialize()
         except InvestmentFund.DoesNotExist:
             market_information[cnpj] = "No information"
-            logging.info(f"No fund found with '{cnpj}'")
+            logging.info(f"{user}: No fund found with '{cnpj}'")
     return JsonResponse({"data": market_data, "information": market_information}, safe=False)
 
 
